728f9f0e58d85ae0ac74f65182450ba96c8629d6
vagrant <vagrant@precise32.(none)>
2014-01-23 00:12:29 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,48 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("JS is there");
+		$("#testjs").text("button");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(updateProject); 
+}
+
+function projectClick(e) {
+	
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+    
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+
+       $ (description).fadeOut();
+    }
+    
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   console.log(projectID);
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
